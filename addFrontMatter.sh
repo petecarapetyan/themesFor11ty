a40496f25d77181836166fb52b1a4146c218f875
Pete Carapetyan <pete.carapetyan@gmail.com>
2024-06-11 20:01:18 -0500

    WIP

--- addFrontMatter.sh
+++ addFrontMatter.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
+npm install @11ty/eleventy-navigation --save-dev
 # Find all .md files, skipping the node_modules directory
 find "$(pwd)" -path "$(pwd)/node_modules" -prune -o -type f -name "*.md" -print | while read -r file; do
+  # Get the relative path of the file
+  relative_path="${file#$(pwd)/}"
   # Check if the first three non-whitespace characters in the file are not '---'
   if ! head -n 1 "$file" | grep -q '^---'; then
-    # Get the relative path of the file
-    relative_path="${file#$(pwd)/}"
     # Get the filename without the rest of the path and without the .md extension
     filename="$(basename "$file" .md)"
     # Get the parent directory of the file
     parent_dir="$(basename "$(dirname "$file")")"
     # Print the relative path, the filename, and the parent directory
-    echo "$relative_path"
-    echo "$filename"
-    echo "$parent_dir"
+    echo "Added frontmatter to $relative_path"
     # Create the new content to prepend
     new_content="---
 layout: layout-sidebar
@@ -22,6 +20,8 @@ eleventyNavigation:
   key: $filename
   title: $filename
   parent: $parent_dir
+  # order: 42
+# FIXME - Values shown above are currently derived from the file path only, except order which is also commented out because it is optional. Correct as desired and delete comment(s).
 ---
 "
     # Prepend the new content to the file
@@ -29,5 +29,9 @@ eleventyNavigation:
     echo "$new_content" > "$temp_file"
     cat "$file" >> "$temp_file"
     mv "$temp_file" "$file"
+  
+  else
+    # Print the message for files that are not processed
+    echo "Has frontmatter $relative_path"
   fi
 done
